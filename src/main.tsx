d42cab3584e7ae8c9f666014f84b90ad77ac0f1d
Chantal Jenkins <60161241+cjenkin1@users.noreply.github.com>
2024-01-08 14:56:23 -0500

    working on pulling + displaying prereqs

--- src/main.tsx
+++ src/main.tsx
@@ -3,47 +3,88 @@ import * as ReactDOM from "react-dom/client";
 import { dbClient } from "./dbClient";
 import { Database } from "./dbTypes";
 
+type mainCourse = Database["public"]["Tables"]["courses"]["Row"];
+
 ReactDOM.createRoot(document.querySelector("#root")!).render(<App />);
 
 export default function App() {
-	const [c, setC] = React.useState<
-		Database["public"]["Tables"]["courses"]["Row"][]
-	>([]);
+	const [courses, setCourses] = React.useState<mainCourse[]>([]);
 	React.useEffect(() => {
-		getCourses();
+		getAllCourses();
 	}, []);
-	async function getCourses() {
+	async function getAllCourses() {
 		const { data } = await dbClient.from("courses").select();
-		setC(data!);
+		setCourses(data!);
 	}
-	const c0 = c.map((course) => (
+	const c = courses.map((course) => (
 		<CourseComp course={course} key={course["id"]} />
 	));
-	return <>{c0}</>;
+	return <>{c}</>;
 }
 
-function CourseComp(props: {
-	course: Database["public"]["Tables"]["courses"]["Row"];
-}) {
+function CourseComp(props: { course: mainCourse }) {
+	React.useEffect(() => {
+		let ignore: boolean = false;
+		prereqs?.map((p) =>
+			p.map(async (p0) => {
+				if (!isNaN(Number(p0))) {
+					const pre = await getCourse(Number(p0));
+					if (pre) p0 = `${pre[0].subject} ${pre[0].course_number}`;
+				}
+				return p0;
+			})
+		);
+		if (!ignore) {
+			prereqs?.forEach((p) => {
+				if (p.length === 1) formattedPrereqs.push(<li>{p[0]}</li>);
+				else {
+					let str: string = "";
+					p.forEach((p0, i) => {
+						if (i === p.length) str += p0;
+						else str += `${p0} OR`;
+					});
+					formattedPrereqs.push(str);
+				}
+			});
+		}
+
+		return () => {
+			ignore = true;
+		};
+	}, []);
+	const prereqs = props.course.prerequisites
+		?.toString()
+		.split(/,/)
+		.map((p) => p.split(/OR/));
+	const formattedPrereqs = [];
+	async function getCourse(id: number) {
+		const { data } = await dbClient
+			.from("courses")
+			.select("subject, course_number")
+			.eq("id", id);
+		return data;
+	}
 	return (
-		<>
+		<div id="course-box">
 			<strong>{`${props.course["subject"]} ${props.course["course_number"]} - ${props.course["title"]}`}</strong>
 			<hr />
 			<em>{props.course["description"]}</em>
 			<br />
 			<br />
-			<SectionTable sections={props.course["sections"]} />
-		</>
+			<strong>Prerequisites:</strong>
+			<ul></ul>
+			<SectionTable course={props.course} />
+		</div>
 	);
 }
-function SectionTable(props: {
-	sections: Database["public"]["Tables"]["courses"]["Row"]["sections"];
-}) {
-	const s = props.sections?.map((sec) => <tr>{sec}</tr>);
+function SectionTable(props: { course: mainCourse }) {
+	const sections = props.course.sections?.map((section) => (
+		<tr key={section}>{section}</tr>
+	));
 	return (
 		<table>
 			<th>Section</th>
-			{s}
+			<tbody>{sections}</tbody>
 		</table>
 	);
 }
